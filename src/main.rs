5cb2ad2d67612f1d2fe39603a63bd90080e08b18
Guillem Arias Fauste <hello@gariasf.com>
2024-05-24 16:41:39 +0200

    create device, memory allocator, data and buffer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,67 @@
+use std::sync::Arc;
+use vulkano::buffer::{Buffer, BufferContents, BufferCreateInfo, BufferUsage};
+use vulkano::device::{Device, DeviceCreateInfo, QueueCreateInfo, QueueFlags};
+use vulkano::VulkanLibrary;
+use vulkano::instance::{Instance, InstanceCreateInfo};
+use vulkano::memory::allocator::{AllocationCreateInfo, MemoryTypeFilter, StandardMemoryAllocator};
+use vulkano::swapchain::FullScreenExclusive::Default;
+
+#[derive(BufferContents)]
+#[repr(C)]
+struct MyStruct {
+    a: u32,
+    b: u32,
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let library = VulkanLibrary::new().expect("no local Vulkan library/DLL");
+    let instance = Instance::new(library, InstanceCreateInfo::default()).expect("failed to create instance");
+
+    let physical_device = instance
+        .enumerate_physical_devices()
+        .expect("could not enumerate devices")
+        .next()
+        .expect("no devices available");
+
+    let queue_family_index = physical_device.queue_family_properties()
+        .iter().enumerate()
+        .position(|(_queue_family_index, queue_family_properties)| {
+            queue_family_properties.queue_flags.contains(QueueFlags::GRAPHICS)
+        })
+        .expect("Couldn't find graphical queue family") as u32;
+
+    let (device, mut queues) = Device::new(
+        physical_device,
+        DeviceCreateInfo {
+            queue_create_infos: vec![QueueCreateInfo {
+                queue_family_index,
+                ..Default::default()
+            }],
+            ..Default::default()
+        }
+    ).expect("failed to create device");
+
+    let queue = queues.next().unwrap();
+    let memory_allocator = Arc::new(StandardMemoryAllocator::new_default(device.clone()));
+
+    let data = MyStruct { a: 5, b: 69 };
+
+    let buffer = Buffer::from_data(
+        memory_allocator.clone(),
+        BufferCreateInfo {
+            usage: BufferUsage::UNIFORM_BUFFER,
+            ..Default::default()
+        },
+        AllocationCreateInfo {
+            memory_type_filter: MemoryTypeFilter::PREFER_DEVICE | MemoryTypeFilter::HOST_SEQUENTIAL_WRITE,
+            ..Default::defaul()
+        },
+        data
+    ).expect("failed to create buffer");
+
+    let content = buffer.write().unwrap();
+
+
+
 }
