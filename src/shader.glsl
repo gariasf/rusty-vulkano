0b09c0fec39509086131a530272a9098051d403a
Guillem Arias Fauste <hello@gariasf.com>
2024-05-25 16:01:24 +0200

    amend shader comment

--- src/shader.glsl
+++ src/shader.glsl
@@ -2,7 +2,7 @@
 
 layout (local_size_x = 64, local_size_y = 1, local_size_z = 1) in;
 
-// Descriptor. Buffer called buf
+// Descriptor
 layout (set = 0, binding = 0) buffer Data {
     uint data[];
 } buf;
